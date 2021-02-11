edabaabc44c20c6619379ee85fb7c0f7e994cf35
Mikeladels <michelleadelia59008@gmail.com>
2021-02-11 09:37:20 +0700

    master

---
+++ 11 Februari 2021/exponensial.py
@@ -0,0 +1,8 @@
+#michelle adelia suwarno / xi mia 1 / 25
+#exponensial
+
+from math import e, exp, log
+
+print(pow(e , 1) == exp(log(e)))
+print(pow(2 , 2) == exp(2 * log(2)))
+print(pow(e , e) == exp(0))
